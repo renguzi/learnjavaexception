73141306153cbbce214685a91ccf193ca5352a7d
Asher Huang <whuang@onlyou.com>
2019-10-05 23:48:16 +0800

    学习集合中的Map，向Map中添加键值对，单独遍历键值对中的value，以及遍历key-value键值对。

--- HashSet/src/Cat.java
+++ HashSet/src/Cat.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 /**
  * @Author: Asher Huang
  * @Date: 2019-10-05
@@ -48,5 +50,24 @@ public class Cat {
                 ", month=" + month +
                 ", species='" + species + '\'' +
                 '}';
+    }
+
+//    这里的equals和hashCode方法重写的目的，是为了保证在CatTest中，不可以添加重复猫
+    @Override
+    public boolean equals(Object o) {
+//       判断对象是否相等，相等则直接返回true，不用再继续比较其属性了
+        if (this == o) return true;
+//        判断o是否是猫类，不过不是则直接返回false，肯定不相等
+        if (!(o instanceof Cat)) return false;
+//        继续下来，如果o可以转换为猫类，则把其转换成猫类，并且判断其属性是否和当前属性相同
+        Cat cat = (Cat) o;
+        return getMonth() == cat.getMonth() &&
+                Objects.equals(getName(), cat.getName()) &&
+                Objects.equals(getSpecies(), cat.getSpecies());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getMonth(), getSpecies());
     }
 }
--- HashSet/src/CatTest.java
+++ HashSet/src/CatTest.java
@@ -18,6 +18,7 @@ public class CatTest {
         Set set = new HashSet();
         set.add(huahua);
         set.add(fanfan);
+
 //        从HashSet中遍历宠物猫信息
         Iterator iterator = set.iterator();
         while (iterator.hasNext()) {
@@ -28,5 +29,39 @@ public class CatTest {
             System.out.println(iterator.next());
         }
 
+//        添加重复数据，进行测试，创建一支相同的猫
+//        如果不在Cat类中，override equals和hashCode方法的话，则无法保证不能添加重复猫对象
+        Cat huahua01 = new Cat("花花", 12, "英国短毛猫");
+        set.add(huahua01);
+        System.out.println("重复添加猫对象之后：");
+        iterator = set.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+//        从Set中查找
+//        利用对象名称huahua来查找，比较简单
+        if(set.contains(huahua)){
+            System.out.println("huahua找到了");
+            System.out.println(huahua);
+        }else {
+            System.out.println("huahua没找到");
+        }
+
+//        在集合中，使用猫的名称来查找
+        System.out.println("在集合中，使用猫的名称来查找");
+        iterator=set.iterator();
+        Cat cat=null;
+        while (iterator.hasNext()){
+             cat=(Cat)(iterator.next());
+            if (cat.getName()=="花花"){
+                System.out.println("花花找到了");
+                System.out.println(cat);
+                break;
+            }
+            else {
+                System.out.println("花花没找到");
+            }
+        }
     }
 }
