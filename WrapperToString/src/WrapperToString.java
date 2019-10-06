817e42585d82148502863c21da1c0c2f8831e3fc
Asher Huang <whuang@onlyou.com>
2019-10-06 14:52:56 +0800

    基本类型和字符串之间的转换，是靠包装类这个桥梁来完成的。

--- WrapperToString/src/WrapperToString.java
+++ WrapperToString/src/WrapperToString.java
@@ -6,12 +6,14 @@
  */
 public class WrapperToString {
     public static void main(String[] args) {
-//      基本类型转换为字符串
+
+        //      一 基本类型转换为字符串
         int t1=2;
         String t2=Integer.toString(t1);
         System.out.println("基本数据类型int t1="+t1);
         System.out.println("字符串类型String t2="+t2);
-//字符串类型转换为基本类型int
+
+        //      二 字符串类型转换为基本类型int
 //        方法1，通过包装类的parseInt()方法
         int t3= Integer.parseInt(t2);
         System.out.println("字符串转换为int类型t3="+t3);
