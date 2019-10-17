1cdee90063783c0b4c54859fe892ea109c0e0ae6
Asher Huang <whuang@onlyou.com>
2019-10-17 16:29:13 +0800

    通过Spring Expression Language来设置bean的属性注入。

---
+++ spring_ioc/src/main/java/com/imooc/ioc/beanshuxingzhurufangshi/Product.java
@@ -0,0 +1,46 @@
+package com.imooc.ioc.beanshuxingzhurufangshi;
+
+/**
+ * @Author: Asher Huang
+ * @Date: 2019-10-17
+ * @Description: com.imooc.ioc.beanshuxingzhurufangshi
+ * @Version:1.0
+ */
+public class Product {
+    private String name;
+    private Double price;
+    private Category category;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", category=" + category +
+                '}';
+    }
+}
---
+++ spring_ioc/src/main/java/com/imooc/ioc/beanshuxingzhurufangshi/ProductInfo.java
@@ -0,0 +1,14 @@
+package com.imooc.ioc.beanshuxingzhurufangshi;
+
+/**
+ * @Author: Asher Huang
+ * @Date: 2019-10-17
+ * @Description: com.imooc.ioc.beanshuxingzhurufangshi
+ * @Version:1.0
+ */
+public class ProductInfo {
+
+    public Double calcPrice() {
+        return Math.random()*1999.99;
+    }
+}
